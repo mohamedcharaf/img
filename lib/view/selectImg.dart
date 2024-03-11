a896c97300b106a88b7d76d4485efef0fae210d6
the_intelligence_world <theintelligence.46@gmail.com>
2024-03-11 14:58:07 +0000

    je change test to buton

--- lib/view/selectImg.dart
+++ lib/view/selectImg.dart
@@ -10,8 +10,8 @@ import 'package:img/controller/selectImg.dart';
 import 'package:img/view/widgets/customeBodyText.dart';
 import 'package:img/view/widgets/customeImage.dart';
 import 'package:img/view/widgets/customeTitle.dart';
-import 'package:img/view/widgets/test.dart';
 import 'package:lottie/lottie.dart';
+import 'package:img/view/widgets/button.dart';
 
 class SelectImg extends StatelessWidget {
   SelectImg({Key? key, required this.title}) : super(key: key);
@@ -21,40 +21,53 @@ class SelectImg extends StatelessWidget {
   ImageController controller = Get.put(ImageController());
   @override
   Widget build(BuildContext context) {
-   // final UserController userController = UserController(context);
+    // final UserController userController = UserController(context);
     return Scaffold(
-      
-      body: 
-      SafeArea(
-        child: GetBuilder<ImageController>(
-          builder: (controller) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                
-                  CustomeLottieImage(height: 260,path: AppImages.select,),
-                  const CustomeTitle(title:"Choose Image",size: 40,),
-                  const CustomeBodyText(body:  "You can choose the perscription from your gallery or take an image for it")
-                 ,Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Button1(buttonIcon: Icons.photo,
-                      buttonTitle: "Gallery",imagePath: imagePath,function:() {
-                        return controller.afficheImageGallery2(ImageSource.gallery);
-                      },),
-                    const SizedBox(height: 20,),
-                    Button1(buttonIcon: Icons.camera_alt_outlined,
-                      buttonTitle: "Camera", function: () async {
-                      return  controller.afficheImageGallery2(ImageSource.camera); 
-                    },imagePath: imagePath,)
-      
-                  ],
-                ),
-              ],
-            );
-          }
-          
-        ),
+      body: SafeArea(
+        child: GetBuilder<ImageController>(builder: (controller) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomeLottieImage(
+                height: 260,
+                path: AppImages.select,
+              ),
+              const CustomeTitle(
+                title: "Choose Image",
+                size: 40,
+              ),
+              const CustomeBodyText(
+                  body:
+                      "You can choose the perscription from your gallery or take an image for it"),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Button1(
+                    buttonIcon: Icons.photo,
+                    buttonTitle: "Gallery",
+                    imagePath: imagePath,
+                    function: () {
+                      return controller
+                          .afficheImageGallery2(ImageSource.gallery);
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Button1(
+                    buttonIcon: Icons.camera_alt_outlined,
+                    buttonTitle: "Camera",
+                    function: () async {
+                      return controller
+                          .afficheImageGallery2(ImageSource.camera);
+                    },
+                    imagePath: imagePath,
+                  )
+                ],
+              ),
+            ],
+          );
+        }),
       ),
     );
   }
